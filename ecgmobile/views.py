1406a18f5aec87b5c3d6cb5a3f7ff99f9bc4aaa6
Ilya Shoshin <galarius.is@gmail.com>
2019-08-16 21:46:44 +0300

    Updated engine, templates & style.

--- ecgmobile/views.py
+++ ecgmobile/views.py
@@ -4,9 +4,14 @@ from flask import render_template, redirect, url_for
 from .app import app, pages
 
 def menu_pages():
-    web_pages = [page for page in pages if 'date' in page.meta]
+    web_pages = [page for page in pages if page.meta['type'] == 'page' and not 'hidden' in page.meta]
     # Sort pages by date
     return sorted(web_pages, key=lambda page: page.meta['date'])
+
+def sections(page):
+    title = page.meta['title']
+    sections = [page for page in pages if page.meta['type'] == 'section' and page.meta['page'] == title]
+    return sorted(sections, key=lambda page: page.meta['id'])
 
 @app.route('/')
 def home():
@@ -17,4 +22,5 @@ def page(path):
     # Path is the filename of a page, without the file extension
     # e.g. "first-page"
     page = pages.get_or_404(path)
-    return render_template('page.html', pages=menu_pages(), page=page)
+    page_sections = sections(page)
+    return render_template('page.html', page=page, pages=menu_pages(), sections=page_sections, enumerate=enumerate, len=len)
